f8316f22187c554c7ad454f130810dc6e515944c
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2024-07-24 12:12:59 -0300

    hey-5: Finishing tests and question criation

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use Closure;
 use Illuminate\Http\{RedirectResponse, Request};
 
 class QuestionController extends Controller
@@ -13,7 +14,15 @@ class QuestionController extends Controller
 
         Question::query()->create(
             request()->validate([
-                'question' => ['required'],
+                'question' => [
+                    'required',
+                    'min:10',
+                    function (string $attribute, mixed $value, Closure $fail) {
+                        if ($value[strlen($value) - 1] != '?') {
+                            $fail('Are you sure that is a question? It is missing the question mark in the end.');
+                        }
+                    },
+                ],
             ])
         );
 
--- tests/Feature/CreateQuestionTest.php
+++ tests/Feature/CreateQuestionTest.php
@@ -21,9 +21,27 @@ it("should be able to create a question bigger than 255 characters", function ()
 });
 
 it("should check if question ends with ? mark", function () {
+    $user = User::factory()->create();
+    actingAs($user);
 
-})->todo();
+    $request = post(route("question.store"), [
+        'question' => str_repeat('*', 20),
+    ]);
+
+    $request->assertSessionHasErrors([
+        'question' => 'Are you sure that is a question? It is missing the question mark in the end.',
+    ]);
+    assertDatabaseCount('questions', 0);
+});
 
 it("should have at least 10 characters", function () {
+    $user = User::factory()->create();
+    actingAs($user);
 
-})->todo();
+    $request = post(route("question.store"), [
+        'question' => str_repeat('*', 8) . '?',
+    ]);
+
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    assertDatabaseCount('questions', 0);
+});
