3ef1cad74152b594bf776ca8c7381821dce3b4d0
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2024-07-25 14:56:40 -0300

    hey-8: FEAT: Creating tests, controller and view for questions

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -2,3 +2,6 @@
     'question'
 ])
 
+<div class="rounded dark:bg-gray-800/50 dark:text-gray-400 bg-white shadow-lg p-3 text-black mb-4">
+    {{ $question->question }}
+</div>
---
+++ tests/Feature/ListQuestionsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\{Question, User};
+
+use function Pest\Laravel\{actingAs, get};
+
+it('should list all the questions', function () {
+    $questions = Question::factory()->count(5)->create();
+    $user      = User::factory()->create();
+
+    actingAs($user);
+    $response = get(route('dashboard'));
+
+    /** @var Question $q */
+    foreach ($questions as $q) {
+        $response->assertSee($q->question);
+    }
+
+});
